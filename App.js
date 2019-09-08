74a9da3f627cc5dbdd3ed4f2d6d49ca952553c5e
devnow8 <72devdev@gmail.com>
2019-09-08 16:00:24 +0900

    Object

--- App.js
+++ App.js
@@ -38,7 +38,12 @@ export default class App extends React.Component {
       />
       <ScrollView contentContainerStyle={styles.toDos}>
         {Object.values(toDos).map(toDo => ( 
-        <ToDo key={toDo.id} {...toDo} deleteToDo={this._deleteToDo} />
+        <ToDo key={toDo.id} 
+        deleteToDo={this._deleteToDo}
+        uncompleteToDo={this._uncompleteToDo}
+        completeToDo={this._completeToDo}
+        {...toDo} 
+         />
         ))}
       </ScrollView>
       </View>
@@ -94,6 +99,36 @@ export default class App extends React.Component {
       }
       return {...newState}
     })
+  }
+  _uncompleteToDo = id => {
+    this.setState(prevState => {
+      const newState = {
+        ...prevState,
+        toDos: {
+          ...prevState.toDos,
+          [id]: {
+            ...prevState.toDos[id],
+            isCompleted: false
+          }
+        }
+      };
+      return { ...newState }
+    })
+  }
+  _completeToDo= id => {
+    this.setState(prevState => {
+      const newState = {
+        ...prevState,
+        toDos: {
+          ...prevState.toDos,
+          [id]: {
+            ...prevState.toDos[id],
+            isCompleted: true
+          }
+        }
+      };
+      return { ...newState }
+    });
   }
 }
 
--- ToDo.js
+++ ToDo.js
@@ -17,12 +17,14 @@ export default class ToDo extends Component{
         text: PropTypes.string.isRequired,
         isCompleted: PropTypes.bool.isRequired,
         deleteToDo: PropTypes.func.isRequired,
-        id: PropTypes.string.isRequired
+        id: PropTypes.string.isRequired,
+        uncompleteToDo: PropTypes.func.isRequired,
+        completeToDo: PropTypes.func.isRequired
     };
     
     render(){
-        const { isCompleted, isEditing, toDoValue } = this.state;
-        const { text, id, deleteToDo } = this.props;
+        const { isEditing, toDoValue } = this.state;
+        const { text, id, deleteToDo, isCompleted } = this.props;
         return(
         <View style={styles.container}>
             <View style={styles.column}>
@@ -33,7 +35,7 @@ export default class ToDo extends Component{
                     styles.uncompletedCircle]}/>
             </TouchableOpacity>
             {isEditing ? (<TextInput 
-            style={[styles.text, 
+            style={[styles.text,
                     styles.input,
                     isCompleted ? styles.completedText : styles.uncompletedText]} 
             value={toDoValue}
@@ -79,11 +81,12 @@ export default class ToDo extends Component{
         );
     }
     _toggleComplete = () => {
-        this.setState(prevState => {
-            return {
-                isCompleted: !prevState.isCompleted
-            };
-        })
+        const { isCompleted, uncompleteToDo, completeToDo, id } = this.props;
+        if(isCompleted){
+            uncompleteToDo(id);
+        }else{
+            completeToDo(id);
+        }
     }
     _startEditing = () => {
         this.setState({
@@ -150,8 +153,9 @@ const styles = StyleSheet.create({
         marginHorizontal: 10
     },
     input:{
-        marginVertical : 10,
+        
         width: width/2,
+        marginVertical: 15,
         paddingBottom: 5
     }
 })
