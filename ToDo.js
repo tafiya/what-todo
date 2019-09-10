6645453822a68155bb61be16dc1f99402bca0a4b
devnow8 <72devdev@gmail.com>
2019-09-10 10:29:25 +0900

    spalsh+icon

--- ToDo.js
+++ ToDo.js
@@ -70,7 +70,7 @@ export default class ToDo extends Component{
                                 <Text style={styles.actionText}>✏️</Text>
                             </View>
                         </TouchableOpacity>
-                        <TouchableOpacity onPressOut={() => deleteToDo(id)}>
+                        <TouchableOpacity onPressOut={(event) => {event.stopPropagation; deleteToDo(id)}}>
                             <View style={styles.actionContainer}>
                                 <Text style={styles.actionText}>❌</Text>
                             </View>
@@ -81,7 +81,8 @@ export default class ToDo extends Component{
         
         );
     }
-    _toggleComplete = () => {
+    _toggleComplete = (event) => {
+        event.stopPropagation();
         const { isCompleted, uncompleteToDo, completeToDo, id } = this.props;
         if(isCompleted){
             uncompleteToDo(id);
@@ -89,12 +90,14 @@ export default class ToDo extends Component{
             completeToDo(id);
         }
     }
-    _startEditing = () => {
+    _startEditing = (event) => {
+        event.stopPropagation();
         this.setState({
             isEditing: true
         });
     }
-    _finishEditing = () => {
+    _finishEditing = (event) => {
+        event.stopPropagation();
         const {toDoValue} = this.state;
         const {id, updateToDo} = this.props;
         updateToDo(id, toDoValue);
